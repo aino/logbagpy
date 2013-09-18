3ebfb531273be63d8fe107e586da72c3673128b8
Mikko Hellsing <mikko@aino.se>
2013-09-18 15:09:05 +0200

    0.0.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ class TestCommand(test):
 
 setup(
     name='logbag',
-    version='0.0.6',
+    version='0.0.7',
     description='Cloud logging.',
     long_description=open('README.rst').read(),
     author='Mikko Hellsing',
